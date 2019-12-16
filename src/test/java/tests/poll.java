838801f5a47c4792d8e09d520bad4d8ba0c6d9cb
Erkan <sercan7090@gmail.com>
2019-12-16 18:24:11 +0100

    update poll file

---
+++ src/test/java/tests/poll.java
@@ -0,0 +1,20 @@
+package tests;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
+public class poll {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Hello World");
+
+    }
+
+
+
+
+
+
+
+}
