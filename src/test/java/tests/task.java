fc922240056e937effe6dbe0950b99077a6d2139
Erkan7090 <sercan7090@gmail.com>
2023-04-11 14:56:50 +0200

    Added Strings

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -4,8 +4,12 @@ public class task {
   
   
   public static void main(String[] args){
-  System.out.println("Hello world")
+  System.out.println("Hello world");
     //this added from github
+
+    String s="Erkan";
+    String d="Sahin";
+    System.out.println("d = " + d);
     
   }
 }
